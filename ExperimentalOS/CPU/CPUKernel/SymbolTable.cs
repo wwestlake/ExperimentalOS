51ca01d835dce4ac33b2a26fa716020ee24653ec
wwestlake <wwestlake@lagdaemon.com>
2014-02-01 10:13:46 -0500

    Added GNU GPL License to code

--- ExperimentalOS/CPU/CPUKernel/SymbolTable.cs
+++ ExperimentalOS/CPU/CPUKernel/SymbolTable.cs
@@ -1,4 +1,22 @@
-﻿using LagDaemon.ExperimentalOS.CPU.CPUKernel.InstructionSet;
+﻿/*
+    ExperimentalOS Copyright (C) 2014  William W. Westlake Jr.
+    wwestlake@lagdaemon.com
+  
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using LagDaemon.ExperimentalOS.CPU.CPUKernel.InstructionSet;
 using LagDaemon.ExperimentalOS.OS.Utilities;
 using System;
 using System.Collections.Generic;
